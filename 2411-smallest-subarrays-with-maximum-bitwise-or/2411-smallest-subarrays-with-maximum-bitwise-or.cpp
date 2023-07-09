3b61dc8d42a5dd4244043f8c12102273432d1a4c
Sireesha18 <116700405+DLSireesha@users.noreply.github.com>
2023-07-09 23:22:15 +0530

    Time: 493 ms (19.08%), Space: 66.3 MB (23.66%) - LeetHub

---
+++ 2411-smallest-subarrays-with-maximum-bitwise-or/2411-smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> smallestSubarrays(vector<int>& nums) {
+        int n=nums.size();
+        vector<queue<int>> loc(30);
+        for(int i=0;i<n;i++){
+            for(int b=0;b<30;b++){
+                if(nums[i] & 1<<b) loc[b].push(i);
+            }
+        }
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            int h=i;
+            for(auto& q:loc){
+                if(q.size() && i>q.front()) q.pop();
+                if(q.size()) h=max(h,q.front());
+            }
+            ans.push_back(h-i+1);
+        }
+        return ans;
+    }
+};
