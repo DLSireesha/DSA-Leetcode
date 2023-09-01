68a4d2158c030ec1bc1fd3989fd0deffaf55cb5c
Sireesha18 <116700405+DLSireesha@users.noreply.github.com>
2023-09-01 19:17:00 +0530

    Time: 6 ms (56.68%), Space: 7.9 MB (65.80%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -3,8 +3,7 @@ public:
     vector<int> countBits(int n) {
         vector<int> dp(n+1);
         dp[0]=0;
-        //dp[1]=1;
-        for(int i=1;i<=n;++i){
+        for(int i=1;i<=n;i++){
             dp[i]=dp[i/2]+i%2;
         }
         return dp;
