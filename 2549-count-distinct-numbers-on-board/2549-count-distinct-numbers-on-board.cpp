9af8e210ddf93be77e314a06eaa1bde6b15f52e8
Sireesha18 <116700405+DLSireesha@users.noreply.github.com>
2023-12-05 09:07:08 +0530

    Time: 4 ms (14.34%), Space: 6.5 MB (33.81%) - LeetHub

---
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool check(vector<int>& v,int m){
+        for(auto i:v){
+            if(m==i){
+                return false;
+            }
+        }
+        return true;
+    }
+    int distinctIntegers(int n) {
+        if(n==0 || n==1) return 1;
+        int count=0;
+        vector<int> v;
+        while(n>1){
+            for(int i=n;i>=2;i--){
+                if(n%i==1 && check(v,i)){
+                    v.push_back(i);
+                }
+            }
+            n=n-1;
+        }
+        return v.size()+1;
+    }
+};
