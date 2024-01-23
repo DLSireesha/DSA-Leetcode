58794ad67fb60b901299300bfe6141968af97cb1
Sireesha18 <116700405+DLSireesha@users.noreply.github.com>
2024-01-23 21:50:47 +0530

    Time: 180 ms (34.23%), Space: 62.6 MB (35.27%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int maxLength(vector<string>& arr, string str = "", int index = 0) {
-	    //Use set to check if the string contains all unique characters
         unordered_set<char>s(str.begin(), str.end());
         if (s.size() != ((int)str.length())) 
             return 0;
